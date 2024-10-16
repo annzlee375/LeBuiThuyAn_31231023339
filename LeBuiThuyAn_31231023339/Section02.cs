06f7142ffe33aa94bf723ae277057a4f7b268b90
annzlee375 <anle.31231023339@st.ueh.edu.vn>
2024-10-17 03:02:52 +0700

    Section_02.cs

--- LeBuiThuyAn_31231023339/Section02.cs
+++ LeBuiThuyAn_31231023339/Section02.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace LeBuiThuyAn_31231023339
 {
-    internal class Section02
+    internal class Section_02
     {
         public static void Main()
         {
             //Question_01();
             //Question_02();
             //Question_03();
-          //Question_04();
-         //Question_05();
-          //Question_06();
-          //Question_07();
-        //Question_08();
-       //Question_09();
-          //Question_10();
+            //Question_04();
+            //Question_05();
+            //Question_06();
+            //Question_07();
+            //Question_08();
+            //Question_09();
+            Question_10();
 
             Console.WriteLine("Press any key to stop");
             Console.ReadKey();
@@ -30,14 +30,14 @@ namespace LeBuiThuyAn_31231023339
         /// </summary>
         public static void Question_01()
         {
-            Console.Write("Enter a number a = ");
+            Console.Write("Enter the first number: a = ");
             int a = int.Parse(Console.ReadLine());
-            Console.Write("Enter a number b = ");
+            Console.Write("Enter the second number: b = ");
             int b = int.Parse(Console.ReadLine());
             int sum = a + b;
             int product = a * b;
-            Console.WriteLine($"{a} + {b} = {sum}");
-            Console.WriteLine($"{a} * {b} = {product}");
+            Console.WriteLine($"The sum of a and b is {a} + {b} = {sum}");
+            Console.WriteLine($"The product of a and b is {a} * {b} = {product}");
         }
 
         /// <summary>
@@ -45,14 +45,14 @@ namespace LeBuiThuyAn_31231023339
         /// </summary>
         public static void Question_02()
         {
-            Console.Write("Enter a number a = ");
+            Console.Write("Enter the first variable: a = ");
             int a = int.Parse(Console.ReadLine());
-            Console.Write("Enter a number b = ");
+            Console.Write("Enter the second variable: b = ");
             int b = int.Parse(Console.ReadLine());
             int swap_a = b;
             int swap_b = a;
-            Console.WriteLine($"The swap value of a = {swap_a}");
-            Console.WriteLine($"The swap value of b = {swap_b}");
+            Console.WriteLine($"The swapped value of a = {swap_a}");
+            Console.WriteLine($"The swapped value of b = {swap_b}");
         }
 
         /// <summary>
@@ -60,12 +60,12 @@ namespace LeBuiThuyAn_31231023339
         /// </summary>
         public static void Question_03()
         {
-            Console.Write("Enter a number a = ");
+            Console.Write("Enter the first floating point number: a = ");
             double a = double.Parse(Console.ReadLine());
-            Console.Write("Enter a number b = ");
+            Console.Write("Enter the second floating point number: b = ");
             double b = double.Parse(Console.ReadLine());
             double multiply = a * b;
-            Console.WriteLine($"{a} * {b} = {multiply}");
+            Console.WriteLine($"The result of a * b is {a} * {b} = {multiply}");
         }
 
         /// <summary>
@@ -74,9 +74,9 @@ namespace LeBuiThuyAn_31231023339
         public static void Question_04()
         {
             Console.Write("Enter a value in feet = ");
-            double a = double.Parse(Console.ReadLine());
-            double meter = a * 0.3048;
-            Console.WriteLine($"Convert {a} feet to {meter} meter");
+            double feet = double.Parse(Console.ReadLine());
+            double meter = feet * 0.3048;
+            Console.WriteLine($"Convert {feet} feet to meter: {feet} * 0.3048 = {meter} meter");
         }
 
         /// <summary>
@@ -85,10 +85,10 @@ namespace LeBuiThuyAn_31231023339
         public static void Question_05()
         {
             Console.Write("Enter a value in Celsius = ");
-            double a = double.Parse(Console.ReadLine());
-            double fahrenheit = (a * 9 / 5) + 32;
-            Console.WriteLine($"Convert {a} Celsius to {fahrenheit} Fahrenheit");
-            Console.WriteLine($"Convert {fahrenheit} Fahrenheit to {a} Celsius");
+            double celsious = double.Parse(Console.ReadLine());
+            double fahrenheit = (celsious * 9 / 5) + 32;
+            Console.WriteLine($"Convert {celsious} Celsius to Fahrenheit: ({celsious} * 9/5) + 32 = {fahrenheit} Fahrenheit");
+            Console.WriteLine($"Convert {fahrenheit} Fahrenheit to Celsius: ({fahrenheit} - 32) * 5/9 = {celsious} Celsious");
         }
 
         /// <summary>
@@ -96,7 +96,16 @@ namespace LeBuiThuyAn_31231023339
         /// </summary>
         public static void Question_06()
         {
-
+            int sizeofint = sizeof(int);
+            int sizeoffloat = sizeof(float);
+            int sizeofdouble = sizeof(double);
+            int sizeofchar = sizeof(char);
+            int sizeofbool = sizeof(bool);
+            Console.WriteLine($"The size of int: {sizeofint} bytes");
+            Console.WriteLine($"The size of float: {sizeoffloat} bytes");
+            Console.WriteLine($"The size of double: {sizeofdouble} bytes");
+            Console.WriteLine($"The size of char: {sizeofchar} bytes");
+            Console.WriteLine($"The size of bool: {sizeofbool} bytes");
         }
 
         /// <summary>
@@ -104,7 +113,11 @@ namespace LeBuiThuyAn_31231023339
         /// </summary>
         public static void Question_07()
         {
-
+            Console.Write("Enter a character: ");
+            char character = Console.ReadKey().KeyChar;
+            int asciivalue = (int)character;
+            Console.WriteLine();
+            Console.WriteLine($"The ASCII value of {character} is: {asciivalue}");
         }
 
         /// <summary>
@@ -113,10 +126,10 @@ namespace LeBuiThuyAn_31231023339
         public static void Question_08()
         {
             Console.Write("Enter the radius r = ");
-            double r = double.Parse(Console.ReadLine());    
+            double r = double.Parse(Console.ReadLine());
             const double PI = 3.14;
             double circle = PI * (r * r);
-            Console.WriteLine($"Calculate Area of Circle with the radius r = {circle}");
+            Console.WriteLine($"Calculate Area of Circle with the radius r: {PI} * ({r} * {r}) = {circle}");
         }
 
         /// <summary>
@@ -127,23 +140,22 @@ namespace LeBuiThuyAn_31231023339
             Console.Write("Enter the side a of the square = ");
             double a = double.Parse(Console.ReadLine());
             double square = a * a;
-            Console.WriteLine($"Calculate Area of Square with the side a = {square}");
+            Console.WriteLine($"Calculate Area of Square with the side a: {a} * {a} = {square}");
         }
 
         /// <summary>
-        /// 10. to convert days to years, weeks and days
+        /// 10. to convert days to years, weeks, and days
         /// </summary>
         public static void Question_10()
         {
             Console.Write("Enter the number of days: ");
-            int day= int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
             int years = day / 365;
-            int day_w = day - years*365;
+            int day_w = day - years * 365;
             int weeks = day_w / 7;
             int d = day_w % 7;
-            Console.WriteLine($"{day} is approximately {years} years, {weeks} weeks, and {day}
+            Console.WriteLine($"{day} is approximately {years} years, {weeks} weeks, and {d} days");
         }
-
-
     }
 }
+
